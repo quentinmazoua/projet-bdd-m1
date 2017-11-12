868099dd405cf119d8a236024d3b8c943f263c78
quentinmazoua <noune03@gmail.com>
2017-11-12 19:08:37 +0100

    Formated dataset

--- Requetes/test.js
+++ Requetes/test.js
@@ -1,7 +1,7 @@
 var conn = new Mongo();
 var db = conn.getDB("affairesCorruption");
 
-cursor = db.test.find()
+cursor = db.test.find({'fields':'departement'})
 while ( cursor.hasNext() )
 {
     printjson( cursor.next() );
